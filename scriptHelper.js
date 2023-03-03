478e3cd26c9645f7565ab4b184645a4dcaf65c80
Linda Schasch <lasch02@sbcglobal.net>
2023-03-03 15:58:42 -0600

    maybe???

--- scriptHelper.js
+++ scriptHelper.js
@@ -60,7 +60,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             launchStatus.style.color = "#419F6A"; //green
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
             fuelStatus.innerHTML = `Fuel level high enough for launch`;
-            launchStatus.innerHTML = `Shuttle is Ready for Launch`;
+            launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
             cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
             launchStatus.style.color = "#C7254E"; //red
         } else if (fuelLevel < 10000 && cargoLevel <= 10000) {
